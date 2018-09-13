f6317334258928aaf1738ef59ef4082e953b7bc3
Yun-Tse Tsai <yuntse@slac.stanford.edu>
2018-09-12 23:09:56 -0700

    add the pulse height/baseline histogram

--- MakeHistograms.py
+++ MakeHistograms.py
@@ -13,12 +13,39 @@ def BookHistograms( hNames, nRows, nChimneysARow, nConnections, nChannels ):
   return h
 # BookHistograms()
 
+
+def MakePlots( plotDir, pList, h ):
+
+  for hName in pList:
+    c = ROOT.TCanvas( hName, hName, 1600, 1200 )
+    ROOT.gStyle.SetOptStat(0)
+
+    if hName in [ 'PosPeak' ]:
+      h[hName].GetZaxis().SetRangeUser( 0, 1 )
+      h[hName].SetTitle("Positive Pulse Height")
+    elif hName in [ 'Baseline' ]:
+      h[hName].GetZaxis().SetRangeUser( 1.5, 2.5 )
+    elif hName in [ 'RMS' ]:
+      h[hName].GetZaxis().SetRangeUser( 0, 0.02 )
+    elif hName in [ 'PosPeakToBaseline' ]:
+      h[hName].GetZaxis().SetRangeUser( 0, 0.5 )
+      h[hName].SetTitle("Positive Pulse Height / Baseline")
+    
+    h[hName].Draw("COLZ")
+    pName = '%s/%s.pdf' % ( plotDir, hName )
+    c.SaveAs( pName )
+
+  return
+
+# MakePlots
+
+
 if __name__ == "__main__":
   
   parser = argparse.ArgumentParser( description='Create a ROOT file containing histograms with the statistics information.' )
   parser.add_argument( 'inFiles', metavar = 'i', type = str, nargs = '+', help = 'the input files.' )
   parser.add_argument( '-o', '--outputfile', dest = 'outFile', type = str, help = 'the output file name.' )
-  # parser.add_argument( '-p', '--plotdir', dest = 'plotDir', type = str, help = 'the directory of output plots.' )
+  parser.add_argument( '-p', '--plotdir', dest = 'plotDir', type = str, help = 'the directory of output plots.' )
 
   args = parser.parse_args()
 
@@ -32,7 +59,8 @@ if __name__ == "__main__":
   nPositions    = 8
   nChannels     = nPositions*4
 
-  hNames = { 'AbsPeak': 'AbsPeak', 'Baseline': 'Baseline', 'RMS': 'RMS', 'PosPeak': 'Peak', 'NegPeak': 'Dip', 'Maximum': 'Maximum', 'Minimum': 'Minimum', 'AbsPeakRMS': 'AbsPeakErr', 'PosPeakRMS': 'PeakErr', 'NegPeakRMS': 'DipErr' }
+  hNames = { 'AbsPeak': 'AbsPeak', 'Baseline': 'Baseline', 'RMS': 'RMS', 'PosPeak': 'Peak', 'NegPeak': 'Dip', 'Maximum': 'Maximum', 'Minimum': 'Minimum', 'AbsPeakRMS': 'AbsPeakErr', 'PosPeakRMS': 'PeakErr', 'NegPeakRMS': 'DipErr', 'PosPeakToBaseline': 'PosPeakToBaseline', 'NegPeakToBaseline': 'NegPeakToBaseline', 'AbsPeakToBaseline': 'AbsPeakToBaseline' }
+  pList = [ 'PosPeak', 'Baseline', 'RMS', 'PosPeakToBaseline' ]
   hList  = BookHistograms( hNames, len(rows), nChimneysARow, nConnections, nChannels )
 
   isXLabeled = [ False ]* ( len(rows)*nChimneysARow )
@@ -48,7 +76,18 @@ if __name__ == "__main__":
     YBinLabel = 'Cable %02d' % ( i.Connection )
 
     for hName in hList.keys():
-      value = eval( 'i.%s' % hNames[hName] )
+      value = 0.
+      if hName in [ 'PosPeakToBaseline', 'NegPeakToBaseline', 'AbsPeakToBaseline' ]:
+        variable = hName[0:7]
+        numerator = eval( 'i.%s' % hNames[variable] )
+        denominator = i.Baseline
+        if denominator < 1.e-9:
+          value = 0.
+        else:
+          value = numerator/denominator
+      else:
+        value = eval( 'i.%s' % hNames[hName] )
+        
       hList[hName].SetBinContent( iXBin, iYBin, value )
       hList[hName].GetXaxis().SetBinLabel( iXBin, XBinLabel )
       if i.Channel == 1:
@@ -59,3 +98,6 @@ if __name__ == "__main__":
     hList[hName].Write()
 
   f.Write()
+
+  if args.plotDir:
+    MakePlots( args.plotDir, pList, hList )
