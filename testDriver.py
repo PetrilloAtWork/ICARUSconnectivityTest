8d21d8d9dac449ff7a139dd3b5f836da5f3ff58b
Gianluca Petrillo <petrillo@slac.stanford.edu>
2018-12-20 04:03:33 -0600

    Version bump (6.0.0).

--- testDriver.py
+++ testDriver.py
@@ -12,7 +12,7 @@ So far, only python environment stuff is usable interactively
 (see `ChimneyReader`), but running from python is still quite better.
 """
 
-__version__ = "5.3.1"
+__version__ = "6.0.0"
 
 # TODO:
 # * `ChimneyReader.start()` warn if the chimney has already been completed
