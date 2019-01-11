cbc6d8e76b4acb3ef68ccf61aa210551c2769e7c
Yun-Tse Tsai <yuntse@slac.stanford.edu>
2019-01-10 16:07:51 -0800

    update the script for the horizontal wires

--- MakeHistograms.py
+++ MakeHistograms.py
@@ -13,10 +13,23 @@ def BookHistograms( hNames, nRows, nChimneysARow, nConnections, nChannels ):
   return h
 # BookHistograms()
 
+def BookHistogramsForHorizontalWires( hNames, nRows, nChimneyARow, nConnections, nChannels ):
+  
+  h = {}
+  nXBins = nRows * 2 * nChimneyARow
+  nYBins = nConnections * nChannels
+  for hName in hNames.keys():
+    hName = 'Horizontal%s' % hName
+    h[hName] = ROOT.TH2F( hName, hName, nXBins, 0, nXBins, nYBins, 0, nYBins )
+  
+  return h
+# BookHistogramsForHorizontalWires()
 
-def MakePlots( plotDir, pList, h ):
+def MakePlots( plotDir, pList, h, doHorizontal = False ):
 
   for hName in pList:
+    if doHorizontal:
+      hName = 'Horizontal%s' % hName
     c = ROOT.TCanvas( hName, hName, 1600, 1200 )
     ROOT.gStyle.SetOptStat(0)
     c.SetRightMargin( 0.12 )
@@ -24,11 +37,21 @@ def MakePlots( plotDir, pList, h ):
     if hName in [ 'PosPeak' ]:
       h[hName].GetZaxis().SetRangeUser( 0, 0.5 )
       h[hName].SetTitle("Positive Pulse Height")
+    elif hName in [ 'HorizontalPosPeak' ]:
+      h[hName].GetZaxis().SetRangeUser( 0, 0.25 )
+      h[hName].SetTitle("Positive Pulse Height of Horizontal Wires")
     elif hName in [ 'Baseline' ]:
-      h[hName].GetZaxis().SetRangeUser( -0.04, 0.02 )
+      h[hName].GetZaxis().SetRangeUser( -0.05, 0.03 )
+    elif hName in [ 'HorizontalBaseline' ]:
+      h[hName].GetZaxis().SetRangeUser( -0.05, 0.2 )
+      h[hName].SetTitle("Baseline of Horizontal Wires")
     elif hName in [ 'RMS' ]:
       h[hName].GetZaxis().SetRangeUser( 0, 0.05 )
-    elif hName in [ 'PosPeakToBaseline' ]:
+    elif hName in [ 'HorizontalRMS' ]:
+      h[hName].GetZaxis().SetRangeUser( 0, 0.15 )
+      h[hName].SetTitle("RMS of Horizontal Wires")
+      
+    elif hName in [ 'PosPeakToBaseline', 'HorizontalPosPeakToBaseline' ]:
       h[hName].GetZaxis().SetRangeUser( 0, 0.5 )
       h[hName].SetTitle("Positive Pulse Height / Baseline")
     
@@ -47,6 +70,7 @@ if __name__ == "__main__":
   parser.add_argument( 'inFiles', metavar = 'i', type = str, nargs = '+', help = 'the input files.' )
   parser.add_argument( '-o', '--outputfile', dest = 'outFile', type = str, help = 'the output file name.' )
   parser.add_argument( '-p', '--plotdir', dest = 'plotDir', type = str, help = 'the directory of output plots.' )
+  parser.add_argument( '-t', '--horizontal', dest = 'doHorizontal', type = bool, help = 'whether we want to make plots for horizontal wires.' )
 
   args = parser.parse_args()
 
@@ -59,11 +83,14 @@ if __name__ == "__main__":
   nConnections  = 18
   nPositions    = 8
   nChannels     = nPositions*4
+  nChimneyARowHorizontal = 2
+  nConnectionsHorizontal = 33
 
   hNames = { 'AbsPeak': 'AbsPeak', 'Baseline': 'Baseline', 'RMS': 'RMS', 'PosPeak': 'Peak', 'NegPeak': 'Dip', 'Maximum': 'Maximum', 'Minimum': 'Minimum', 'AbsPeakRMS': 'AbsPeakErr', 'PosPeakRMS': 'PeakErr', 'NegPeakRMS': 'DipErr', 'PosPeakToBaseline': 'PosPeakToBaseline', 'NegPeakToBaseline': 'NegPeakToBaseline', 'AbsPeakToBaseline': 'AbsPeakToBaseline' }
   pList = [ 'PosPeak', 'Baseline', 'RMS' ]
   # pList = [ 'PosPeak', 'Baseline', 'RMS', 'PosPeakToBaseline' ]
   hList  = BookHistograms( hNames, len(rows), nChimneysARow, nConnections, nChannels )
+  htList = BookHistogramsForHorizontalWires( hNames, len(rows), nChimneyARowHorizontal, nConnections, nChannels )
 
   isXLabeled = [ False ]* ( len(rows)*nChimneysARow )
   isYLabeled = [ False ]* ( nConnections*nChannels )
@@ -72,10 +99,18 @@ if __name__ == "__main__":
     # print 'Chimney: %s' % i.Chimney
     row = i.Chimney[0:2]
     iChimney = int( i.Chimney[2:4] )
-    iXBin = rows.index( row )*nChimneysARow + iChimney
-    iYBin = ( i.Connection - 1 )*nChannels + i.Channel
-    XBinLabel = '%s%02d' % ( row, iChimney )
-    YBinLabel = 'Cable %02d' % ( i.Connection )
+    if args.doHorizontal and ( iChimney == 1 or iChimney == 20 ):
+      iSubX = i.Connection/ ( nConnections + 1 )
+      iXBin = rows.index( row )*2*nChimneyARowHorizontal + int( iChimney == 20 )*2 + iSubX + 1
+      iYBin = ( i.Connection - iSubX * nConnections - 1 )*nChannels + i.Channel
+      # print row, rows.index( row ), iChimney, i.Connection, i.Channel, iXBin, iYBin
+      XBinLabel = '%s%02d-%d' %( row, iChimney, iSubX )
+      YBinLabel = 'Cable %02d' % ( i.Connection )
+    else:
+      iXBin = rows.index( row )*nChimneysARow + iChimney
+      iYBin = ( i.Connection - 1 )*nChannels + i.Channel
+      XBinLabel = '%s%02d' % ( row, iChimney )
+      YBinLabel = 'Cable %02d' % ( i.Connection )
 
     for hName in hList.keys():
       value = 0.
@@ -89,17 +124,29 @@ if __name__ == "__main__":
           value = numerator/denominator
       else:
         value = eval( 'i.%s' % hNames[hName] )
-        
-      hList[hName].SetBinContent( iXBin, iYBin, value )
-      hList[hName].GetXaxis().SetBinLabel( iXBin, XBinLabel )
+
+      if args.doHorizontal and ( iChimney == 1 or iChimney == 20 ):
+        htName = 'Horizontal%s' % hName
+        htList[htName].SetBinContent( iXBin, iYBin, value )
+        htList[htName].GetXaxis().SetBinLabel( iXBin, XBinLabel )
+      else:
+        hList[hName].SetBinContent( iXBin, iYBin, value )
+        hList[hName].GetXaxis().SetBinLabel( iXBin, XBinLabel )
       if i.Channel == 1:
-        hList[hName].GetYaxis().SetBinLabel( iYBin, YBinLabel )
-        
+        if args.doHorizontal and ( iChimney == 1 or iChimney == 20 ) and ( i.Connection/ ( nConnections + 1 ) == 0 ):
+          htList[htName].GetYaxis().SetBinLabel( iYBin, YBinLabel )
+        elif ( iChimney > 1 ) and ( iChimney < 20 ):
+          hList[hName].GetYaxis().SetBinLabel( iYBin, YBinLabel )
+
 
   for hName in hList.keys():
     hList[hName].Write()
+  for hName in htList.keys():
+    htList[hName].Write()
 
   f.Write()
 
   if args.plotDir:
     MakePlots( args.plotDir, pList, hList )
+    if args.doHorizontal:
+      MakePlots( args.plotDir, pList, htList, args.doHorizontal )
